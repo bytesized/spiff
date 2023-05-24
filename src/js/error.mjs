7a0a2982b3cd5896ab5755563e2cbea9448692b4
bytesized <bytesized@users.noreply.github.com>
2023-05-24 01:30:20 -0400

    Improve message when token not recognized

--- src/js/error.mjs
+++ src/js/error.mjs
@@ -1,9 +1,15 @@
 import * as m_popup from "./popup.mjs";
 
 export async function show_api_failure_popup(response) {
+  let title = "API Error";
+  let message = response.error_message;
+  if (response?.payload?.error?.code == 401) {
+    title = "Agent Error";
+    message = "Agent data not found";
+  }
   await m_popup.show({
-    title: "API Error",
-    message: response.error_message,
+    title,
+    message,
     buttons: [m_popup.e_button.ok],
     allow_non_button_close: true,
     button_activated_by_enter_key: m_popup.e_button.ok,
