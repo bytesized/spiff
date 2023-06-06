b9cfbf210c8ff30071299613640a5ee5ca21c4cb
bytesized <bytesized@users.noreply.github.com>
2023-06-06 14:47:24 -0700

    Add auth token button to agent page

--- src/js/page/agent.mjs
+++ src/js/page/agent.mjs
@@ -1,4 +1,5 @@
 import * as m_agent from "../agent.mjs";
+import * as m_popup from "../popup.mjs";
 import * as m_storage from "../storage.mjs";
 
 const k_no_agent_selected_message = "<no agent selected>";
@@ -22,4 +23,12 @@ export async function init() {
     m_storage.change_el_text_callback("agent_page_credits", k_no_agent_selected_message),
     {run_immediately: true}
   );
+  document.getElementById("auth_token_view_button").addEventListener("click", async event => {
+    return m_popup.show({
+      title: "Auth Token",
+      readonly_textarea: m_agent.k_current.auth_token.get() ?? k_no_agent_selected_message,
+      buttons: [m_popup.e_button.ok],
+      allow_non_button_close: true,
+    });
+  });
 }
